8c3b340128b5824718dd5e3a83208c635bd7e5b3
WillerLucoles <willerlob@gmail.com>
2024-11-09 00:25:17 -0300

    Creating asset folder Inserting assets Creating the menu: background, sound and fonts Creating the const file

---
+++ Code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_VIVIDSKYBLUE = (85 , 205 , 241)
+COLOR_WHITE = (255 , 255 , 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P' ,
+               'NEW GAME 2P - COOPERATIVE' ,
+               'NEW GAME 2P - COMPETITIVE' ,
+               'SCORE' ,
+               'EXIT'
+               )
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 354
--- Menu.py
+++
@@ -1,7 +0,0 @@
-
-class Menu:
-    def __init__(self, window):
-        seçf;window = window
-
-        def run(self, ):
-            pass
