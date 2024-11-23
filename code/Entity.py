cddf1a30e6102ea6d3414544877a8ffabc6fb924
WillerLucoles <willerlob@gmail.com>
2024-11-22 23:19:04 -0300

    Created and Config Entity.py

--- code/Entity.py
+++ code/Entity.py
@@ -1,8 +1,17 @@
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surf = None
-        self.rect = None
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-    def move(self, ):
+import pygame.image
+
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+
+
+    @abstractmethod
+    def move(self):
         pass
